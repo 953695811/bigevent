c2fc72701f01f6a8feb6e034a434c5656ab57f3c
Node wong <953695811@qq.com>
2020-11-03 18:18:10 +0800

    设置了首页退出按钮和验证方法

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,45 @@
-$.ajaxPrefilter(function(option){
-    console.log(option);
+$(function () {
+    getUserMsg()
+    $('#exit').on('click', function () {
+        layer.confirm('确定退出吗？', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
 })
+
+function getUserMsg() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status === 1) {
+                return
+            } else {
+                userImg(res)
+            }
+        },
+        complete: function (res) {
+            if (res.responseJSON.status !== 0 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token')
+                location.href = '/login.html'
+            }
+        }
+    })
+}
+
+function userImg(res) {
+    $('#welcome').html(res.data.username)
+    if (res.data.user_pic !== null) {
+        $('.layui-nav-img').attr('src', res.data.user_pic)
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(res.data.username.charAt(0).toUpperCase())
+    }
+}
