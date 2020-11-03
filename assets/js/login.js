cfdcdcdfa4e68ab3898ed1405dfff31b9eac43d1
Node wong <953695811@qq.com>
2020-11-03 10:56:26 +0800

    注册功能js代码

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,13 +7,26 @@ $(function(){
         $('.login-box').show()
         $('.reg-box').hide()
     })
+    var form = layui.form
+    form.verify({
+        pwd:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+        repwd: function(value){ //value：表单的值、item：表单的DOM对象
+            if($('.reg-box [name=repassword]').val()!==value){
+              return '两次密码不一致';
+              console.log(1);
+            }
+          }
+    })
+    
     var layer = layui.layer;
     $('#form_login').on('submit',function(e){
         e.preventDefault()
         var msg = $(this).serialize()
         $.post('/api/login',msg,function(res){
             layer.msg(res.message);
-            location.href = '../../index.html'
+            console.log(res);
+            localStorage.setItem('token',res.token)
+            location.href = 'index.html'
         })
         
     })
@@ -22,7 +35,7 @@ $(function(){
         if($('#pwd').val()!==$('#repwd').val()) return layer.msg('两次密码输入不一致，请重新输入');
         var msg = $(this).serialize()
         $.post('/api/reguser',msg,function(res){
-            layer.msg(res.message);
+            layer.msg(res.message); 
         })
         
     })
